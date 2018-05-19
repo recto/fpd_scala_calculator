665c1415bf46b7a1c441a4f3e24f5d71bf38aca7
Itsuo Okamoto <itsuo.okamoto@recto.com>
2018-05-18 21:57:21 -0700

    final

--- src/main/scala/calculator/Polynomial.scala
+++ src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,18 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal (b() * b() - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Signal(
+      if (delta() < 0) Set()
+      else {
+        val sol = (- b() - Math.sqrt(delta())) / (2 * a())
+        val sol2 = (- b() + Math.sqrt(delta())) / (2 * a())
+        Set(sol, sol2)
+      }
+    )
   }
 }
